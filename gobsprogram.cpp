6e5948e548d5070fafb83ef818f412e4b23501d8
James Meneghello <murodese@gmail.com>
2014-07-25 15:34:21 +0800

    Create gobsprogram.cpp

---
+++ gobsprogram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() 
+{
+  cout << "Gob's Program:  Y/N?" << endl << "? ";
+  getchar();
+  while(1)
+  {
+    cout << "Penus  ";
+  }
+}
