66a6b4a9f9cae0cd8f67465b3970e5cb0074eed4
Mattie <spyn@iinet.net.au>
2014-07-26 12:21:31 +0800

    Gob's program in ruby

---
+++ gobsprogram.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+gobs = ""
+until gobs == "N" or gobs == "n"
+  puts "Gob's Program: Y/N? "
+  print "$ "
+  STDOUT.flush
+  gobs = gets.chomp
+  if gobs == "Y" or gobs == "y"
+    until 1 == 0
+      print "Penus "
+      STDOUT.flush
+    end
+  end
+end
