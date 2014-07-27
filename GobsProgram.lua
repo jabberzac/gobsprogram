745790c87cbca2b40299a647f34b3a628f2d2e9e
AlfonZ42 <AlfonZ42@gmail.com>
2014-07-27 14:23:40 +0200

    Lua version

---
+++ GobsProgram.lua
@@ -0,0 +1,11 @@
+local s = nil
+while s ~= "n" and s~= "no" do
+  print("Gob's Program:  Y/N?")
+  io.write("? ")
+  s = string.lower(io.read("*l"))
+  if s == "y" or s == "yes" then
+    while true do
+      io.write("Penus  ")
+    end
+  end
+end
