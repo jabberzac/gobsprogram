c4733028c2dff6feec993090f7d0d79fada8a3c7
larrybrid <jcameronfulton@gmail.com>
2014-07-27 01:08:36 -0400

    Made golang implementation accept either y or Y as valid answers and cleaned up the colors a bit.

--- gobsprogram.go
+++ gobsprogram.go
@@ -1,16 +1,17 @@
 package main
 
 import "fmt"
+import "strings"
 
 func main() {
 	var answer string
-	fmt.Print("\033[1;32m")
-	fmt.Print("\033[40m")
+
+	fmt.Print("\033[1;32;40m")
 	fmt.Println("Gob's Program:  Y/N?")
 	fmt.Print("? ")
 	fmt.Scanf("%s", &answer)
 
-	if answer == "Y" {
+	if strings.ToUpper(answer) == "Y" {
 		prints := 0
 		for true {
 			if prints < 5 {
