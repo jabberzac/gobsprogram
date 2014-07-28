5885e21e6578e3346a479651bf727ee390bde40c
Adam Couper <lokifer@gmail.com>
2014-07-28 23:31:28 +1200

    Added terminal colour, changed to 40 line to be closer to Issue #3

--- gobsprogram.py
+++ gobsprogram.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import platform
+
+
+def os_colour():
+    if platform.system() == 'Windows':
+        print('@echo off')
+        print('color 0A')
+    elif platform.system() == 'Linux':
+        print("\033[1;32;40m")
+
 
 def main():
-	print("Gob's Program:  Y/N?")
-	choice = raw_input('? ')
-	if 'Y' in choice.upper():
-		while True:
-			print('Penus  '*6)
+    os_colour()
+    print("Gob's Program:  Y/N?")
+    choice = raw_input('? ')
+    if choice.upper() == 'Y':
+            while True:
+                print('Penus  ' * 5 + 'Penus')
 
 if __name__ == '__main__':
-	main()
+    main()
