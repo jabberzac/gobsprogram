ca964919cb720442fb47e8eec2421cc75e64d24d
Steve Hoeksema <steve@seven.net.nz>
2014-07-28 12:07:13 +1200

    fit to columns

--- gobsprogram.js
+++ gobsprogram.js
@@ -5,10 +5,23 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
+var cols = process.stdout.columns;
+var line = "";
+
+for (i = 0; (line.length + 7) <= cols; i++) {
+  if (i > 0) {
+    line += "  ";
+  }
+
+  line += "Penus";
+}
+
+line += "\n";
+
 rl.question("Gob's Program:  Y/N?\n? ", function (answer) {
   if (answer.match(/Y/i)) {
     while (true) {
-      process.stdout.write("Penus  ");
+      process.stdout.write(line);
     }
   }
 
